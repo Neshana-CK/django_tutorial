835895ff35e654e683aa1d2a8e2c69d4f260520d
nishanack <nishanack05@gmail.com>
2026-05-12 21:41:29 +0530

    fixed load_data

--- home/management/commands/load_data.py
+++ home/management/commands/load_data.py
@@ -1,11 +1,17 @@
 from django.core.management.base import BaseCommand
 from django.core.management import call_command
+from home.models import Departments
 import os
 
 class Command(BaseCommand):
-    help = 'Load initial data'
+    help = 'Load initial data safely'
 
     def handle(self, *args, **kwargs):
+
+        # Prevent duplicate loading
+        if Departments.objects.exists():
+            self.stdout.write(self.style.WARNING('Data already exists'))
+            return
 
         if os.path.exists('data.json'):
             call_command('loaddata', 'data.json')
--- load_data.py
+++
@@ -1,9 +0,0 @@
-import os
-import django
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_tutorial.settings")
-django.setup()
-
-from django.core.management import call_command
-
-call_command('loaddata', 'data.json')
