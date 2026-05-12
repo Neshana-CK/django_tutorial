317a1ae42ff6243063ef8278960fdefdf638755d
nishanack <nishanack05@gmail.com>
2026-05-12 21:25:35 +0530

    removed loaddata from settings

--- django_tutorial/settings.py
+++ django_tutorial/settings.py
@@ -145,10 +145,3 @@ LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
-from django.core.management import call_command
-
-if os.environ.get("RENDER"):
-    try:
-        call_command('loaddata', 'data.json')
-    except:
-        pass
